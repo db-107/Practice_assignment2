343f7aaf3666e261c51320e3426b2725dfc111b6
db-107 <db107@kent.ac.uk>
2026-03-05 17:25:32 +0000

    Implement password class to manage valid passwords
    Added a class to store and represent valid passwords.

--- Password.py
+++ Password.py
@@ -1,1 +1,9 @@
+#stores valid passwords
 
+class password:
+    def __init__(self, raw: str):
+        self.raw = raw
+    
+def __repr__(self):
+    return f"password(raw='{self.raw}')"
+#returns/ prints valid password in "history.txt"
